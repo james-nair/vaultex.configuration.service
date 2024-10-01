9f085023ba682a85435e3fb6af1806a37ce9f099
James Nair <james.g.nair@outlook.com>
2024-10-01 20:46:17 +1000

    added configuration migration

---
+++ Vaultex.Configuration/Migrations/20241001104447_InitialCreate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vaultex.Configuration.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    Uuid = table.Column<Guid>(type: "uuid", nullable: false),
+                    ParentUuid = table.Column<Guid>(type: "uuid", nullable: true),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    SubType = table.Column<string>(type: "text", nullable: false),
+                    JsonConfig = table.Column<JsonDocument>(type: "jsonb", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.Uuid);
+                    table.ForeignKey(
+                        name: "FK_Settings_Settings_ParentUuid",
+                        column: x => x.ParentUuid,
+                        principalTable: "Settings",
+                        principalColumn: "Uuid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Settings_ParentUuid",
+                table: "Settings",
+                column: "ParentUuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Settings");
+        }
+    }
+}
