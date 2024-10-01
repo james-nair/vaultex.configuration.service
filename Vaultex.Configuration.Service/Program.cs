d7a99d33bfc0457c25914c01fa76b1b3077432c5
James Nair <james.g.nair@outlook.com>
2024-10-01 20:47:14 +1000

    addde config service startup and migrations

--- Vaultex.Configuration.Service/Program.cs
+++ Vaultex.Configuration.Service/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.EntityFrameworkCore;
+using Vaultex.Configuration;
+using Vaultex.Database.Extensions;
+using Vaultex.Database.Options;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddOptions();
+builder.Services.Configure<DatabaseOptions>(builder.Configuration.GetSection(nameof(DatabaseOptions)));
+
+builder.Services.RegisterDbContext<ConfigContext>(options => options.UseNpgsql(DBConnection.ConnectionString("vt_configuration", builder.Configuration).ConnectionString));
+
+
+var host = builder.Build();
+using (var scope = host.Services.CreateScope())
+{
+    var logger = scope.ServiceProvider.GetService<ILoggerFactory>();
+    var log = logger.CreateLogger("ConfigServiceStartup");
+    log.LogInformation("PreMigrate ConfigurationDB");
+    scope.ServiceProvider.CreateAndMigrate<ConfigContext>();
+    log.LogInformation("Migration complete!");
+}
+
+host.Run();
