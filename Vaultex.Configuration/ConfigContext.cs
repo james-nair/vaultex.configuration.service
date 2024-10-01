5c86b73d3fb9e92b14e3c37861171b2b1c0e32b5
James Nair <james.g.nair@outlook.com>
2024-10-01 20:33:53 +1000

    added config db project

---
+++ Vaultex.Configuration/ConfigContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Vaultex.Configuration.Models;
+using Vaultex.Database.Extensions;
+
+namespace Vaultex.Configuration
+{
+    public class ConfigDbContextFactory : IDesignTimeDbContextFactory<ConfigContext>
+    {
+        public ConfigContext CreateDbContext(string[] args)
+        {
+            var optionBuilder = new DbContextOptionsBuilder<ConfigContext>();
+            optionBuilder.UseNpgsql();
+            return new ConfigContext(optionBuilder.Options);
+        }
+    }
+    public class ConfigContext : DbContext
+    {
+        public ConfigContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<SettingDbo> Settings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.AddDateTimeConverter();
+
+            modelBuilder.Entity<SettingDbo>(entity =>
+            {
+                entity
+                .HasOne(a => a.Parent)
+                .WithMany(b => b.Children)
+                .HasForeignKey(nameof(SettingDbo.ParentUuid))
+                .OnDelete(DeleteBehavior.Cascade);
+            });
+        }
+    }
+}
---
+++ Vaultex.Configuration/Models/SettingDbo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
+using Vaultex.Database;
+
+namespace Vaultex.Configuration.Models
+{
+    public class SettingDbo : BaseDbo
+    {
+        public Guid? ParentUuid { get; internal set; }
+
+        [ForeignKey(nameof(ParentUuid))]
+        public virtual SettingDbo? Parent { get; internal set; }
+        public virtual List<SettingDbo>? Children { get; internal set; }
+        public string Type { get; internal set; }
+
+        [Column("SubType")]
+        public string BaseType { get; internal set; }
+        public JsonDocument JsonConfig { get; set; }
+    }
+}
